4790b9f0bf46d75a294edb110eaf97e43577ffc7
Patrick Brogan <pbrogan12@gmail.com>
2014-04-09 21:54:24 -0400

    Follows pep8.

--- mlb.py
+++ mlb.py
@@ -2,12 +2,17 @@ import requests
 import time
 
 # A function that returns scores on a certain day
+
+
 def get_scores(date=time.strftime('%d/%m/%Y')):
     date = date.split("/")
     year = date[2]
     month = date[1]
     day = date[0]
-    url = 'http://mlb.mlb.com/gdcross/components/game/mlb/year_{0}/month_{1}/day_{2}/master_scoreboard.json'.format(year,month,day)
+    url = 'http://mlb.mlb.com/gdcross/components/game/mlb/year_{0}/month_{1}/day_{2}/master_scoreboard.json'.format(
+        year,
+        month,
+        day)
     data = requests.get(url)
     # Initialize json
     data = data.json()
@@ -19,44 +24,48 @@ def get_scores(date=time.strftime('%d/%m/%Y')):
         try:
             homeTeamScore = game['linescore']['r']['home']
             awayTeamScore = game['linescore']['r']['away']
-            inning = game['status']['inning_state'] + ' ' + game['status']['inning']
+            inning = game['status']['inning_state'] + \
+                ' ' + game['status']['inning']
         except:
             homeTeamScore = ''
             awayTeamScore = ''
             inning = game['time_hm_lg']
 
         games[game['game_media']['media']['title']] = {
-            'homeTeamName' : homeTeamName,
-            'awayTeamName' : awayTeamName,
-            'homeTeamScore' : homeTeamScore,
-            'awayTeamScore' : awayTeamScore,
-            'inning' : str(inning)
+            'homeTeamName': homeTeamName,
+            'awayTeamName': awayTeamName,
+            'homeTeamScore': homeTeamScore,
+            'awayTeamScore': awayTeamScore,
+            'inning': str(inning)
         }
 
     return games
 
+
 def get_leaders(limit='10', stat='h', year=time.strftime('%Y')):
     playerDict = {}
     payload = {
-            'season' : year,
-            'sort_order' : '\'desc\'',
-            'sort_column' : '{}'.format(stat),
-            'stat_type' : 'hitting',
-            'page_type' : 'SortablePlayer',
-            'game_type' : '\'R\'',
-            'player_pool' : 'QUALIFIER',
-            'season_type' : 'ANY',
-            'sport_code' : '\'mlb\'',
-            'results' : limit,
-            'recSP' : '1',
-            'recPP': '50'
+        'season': year,
+        'sort_order': '\'desc\'',
+        'sort_column': '{}'.format(stat),
+        'stat_type': 'hitting',
+        'page_type': 'SortablePlayer',
+        'game_type': '\'R\'',
+        'player_pool': 'QUALIFIER',
+        'season_type': 'ANY',
+        'sport_code': '\'mlb\'',
+        'results': limit,
+        'recSP': '1',
+        'recPP': '50'
     }
     url = 'http://mlb.mlb.com/pubajax/wf/flow/stats.splayer'
-    data = requests.get(url,params=payload)
+    data = requests.get(url, params=payload)
     data = data.json()
     for player in data['stats_sortable_player']['queryResults']['row']:
         playerDict[player['name_display_first_last']] = {}
-        playerDict[player['name_display_first_last']]['playerId'] = player['player_id']
+        playerDict[
+            player['name_display_first_last']]['playerId'] = player[
+            'player_id']
         playerDict[player['name_display_first_last']]['team'] = player['team']
         playerDict[player['name_display_first_last']]['bats'] = player['bats']
         playerDict[player['name_display_first_last']]['h'] = player['h']
@@ -64,18 +73,33 @@ def get_leaders(limit='10', stat='h', year=time.strftime('%Y')):
 
     return playerDict
 
-def get_recent(playerId, limit='10',stat='h'):
+
+def get_recent(playerId, limit='10', stat='h'):
     payload = {
-            'results' : limit,
-            'game_type' : '\'R\'',
-            'season' : time.strftime('%Y'),
-            'player_id' : playerId,
-            'mlb_individual_hitting_last_x_total.col_in' : ['game_date','opp','ab','r','h','hr','rbi','bb','so','sb','avg','home_away','game_id','game_type']
-    }
+        'results': limit,
+        'game_type': '\'R\'',
+        'season': time.strftime('%Y'),
+        'player_id': playerId,
+        'mlb_individual_hitting_last_x_total.col_in': [
+            'game_date',
+            'opp',
+            'ab',
+            'r',
+            'h',
+            'hr',
+            'rbi',
+            'bb',
+            'so',
+            'sb',
+            'avg',
+            'home_away',
+            'game_id',
+            'game_type']}
     url = 'http://mlb.mlb.com/lookup/json/named.mlb_bio_hitting_last_10.bam'
-    data = requests.get(url,params=payload)
+    data = requests.get(url, params=payload)
     data = data.json()
-    return data['mlb_bio_hitting_last_10']['mlb_individual_hitting_last_x_total']['queryResults']['row'][stat]
+    return data['mlb_bio_hitting_last_10'][
+        'mlb_individual_hitting_last_x_total']['queryResults']['row'][stat]
 
 if __name__ == '__main__':
     get_scores()
